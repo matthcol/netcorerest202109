1d0df23c155cca4c0a3cf85f50b8293bb7127e35
matthcol <matthias.colin@gmail.com>
2021-09-28 16:25:21 +0200

    new project with dbr

---
+++ WineApiRestDbr/Model/WineDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WineApiRest.Model
+{
+    public class WineDbContext: DbContext
+    {
+        public WineDbContext(DbContextOptions<WineDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Wine> Wines { get; set; }
+    }
+}
