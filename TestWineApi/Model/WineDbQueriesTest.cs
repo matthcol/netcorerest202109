7f69911bafa54de52305b0fb4f2993dd1679b0b2
matthcol <matthias.colin@gmail.com>
2021-09-29 15:31:11 +0200

    first query test

--- TestWineApi/Model/WineDbQueriesTest.cs
+++ TestWineApi/Model/WineDbQueriesTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,25 @@ using Xunit;
 
 namespace TestWineApi.Model
 {
-    public class WineDbQueriesTest
+    public class WineDbQueriesTest: WineDbTest 
     {
+        public WineDbQueriesTest() :
+            base(new DbContextOptionsBuilder<WineDbContext>()
+                .UseInMemoryDatabase("WineDbTest")
+                .Options)
+        { 
+        }
+
+        [Fact]
+        public void test_query_find()
+        {
+            using (var context = new WineDbContext(ContextOptions))
+            {
+                var wines = context.Wines.ToList();
+                Assert.Equal(3, wines.Count);
+            }
+        }
+
         [Fact]
         public void test_query_byMillesime()
         {
